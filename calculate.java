6719dd72bb58e27ed1e10424d9dfd37a895f0e7e
Rinchinovbb <Bargutishman@gmail.com>
2018-06-07 16:14:23 +0600

    create calculate

--- calculate.java
+++ calculate.java
@@ -1,6 +1,22 @@
 public class calculate 
 {
-public static void main(String[] args) {
+public static void main(String[] arg) {
 System.out.println("Calculate...");
+int f = Integer.valueOf(arg[0]);
+int s = Integer.valueOf(arg[1]);
+int su = f+s;
+System.out.println("Sum= "+su);
+int f1 = Integer.valueOf(arg[2]);
+int s2 = Integer.valueOf(arg[3]);
+int sum = f1-s2;
+System.out.println("Sum= "+sum);
+int f3 = Integer.valueOf(arg[4]);
+int s3 = Integer.valueOf(arg[5]);
+int summ = f3*s3;
+System.out.println("Sum= "+summ);
+int f4 = Integer.valueOf(arg[6]);
+int s4 = Integer.valueOf(arg[7]);
+int summ1 = f4/s4;
+System.out.println("Sum= "+summ1);
 }
 }
